ae741e2ba37c1b1041ae9b6e5e6ebc33803869fa
bangwei.xu <bangwei.xu@esmart365.com>
2019-01-24 13:39:01 +0800

    jquery拦截

--- src/page1/index.js
+++ src/page1/index.js
@@ -21,3 +21,27 @@ $(document).ajaxStart(function(m){
 	console.log(err);
 })
 $( document ).ajaxSuccess(function( event, request, settings ) {    console.log('请求状态',request.status);});$( document ).ajaxError(function( event, request, settings ) {    console.log('失败',request.status);   if (request.status == 401) {       alert("没有权限,请登录。");   }});
+$(function(){  
+    $.ajaxSetup({
+        type: "POST",
+        error: function(jqXHR, textStatus, errorThrown) {
+          
+            switch(jqXHR.status) {
+              case(500):
+                alert("服务器系统内部错误");
+                break;
+               case(401):
+                alert("未登录");
+                break;
+               case(403):
+                alert("无权限执行此操作");
+                break;
+               case(408):
+                alert("请求超时");
+                break;
+              default:
+                alert("未知错误");
+             }
+        }
+   });
+});
